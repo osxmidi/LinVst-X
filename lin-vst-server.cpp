a27dd99f3854a7cdda4f5eb15dc57cb284299ef7
osxmidi <chopinbig@tutanota.com>
2020-10-29 17:36:53 +1100

    Update lin-vst-server.cpp

--- lin-vst-server.cpp
+++ lin-vst-server.cpp
@@ -716,7 +716,7 @@ void RemoteVSTServer::EffectOpen()
     {  
 
 
-printf("servup\n");   
+// printf("servup\n");   
     memcpy(&m_shm3[FIXED_SHM_SIZE3], &am, sizeof(am));
 
     writeOpcodering(&m_shmControl->ringBuffer, (RemotePluginOpcode)audioMasterIOChanged);
@@ -1638,7 +1638,7 @@ VstIntPtr RemoteVSTServer::hostCallback2(AEffect *plugin, VstInt32 opcode, VstIn
 #endif
 * */
 
- printf("server %d %d %d \n", am.incount, am.outcount, am.delay);
+ // printf("server %d %d %d \n", am.incount, am.outcount, am.delay);
 
     if((am.incount != m_numInputs) || (am.outcount != m_numOutputs) || (am.delay != m_delay))
     {  
@@ -1651,7 +1651,7 @@ VstIntPtr RemoteVSTServer::hostCallback2(AEffect *plugin, VstInt32 opcode, VstIn
    
   */
   
-  printf("delay\n");
+ // printf("delay\n");
   
     if(am.delay != m_delay)
     m_delay = am.delay;
