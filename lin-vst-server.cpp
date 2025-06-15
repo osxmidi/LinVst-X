4dbffa0a29095779140a5d1c4ac6affc290910f2
osxmidi <chopinbig@tutanota.com>
2025-06-15 20:50:28 +1000

    Update lin-vst-server.cpp

--- lin-vst-server.cpp
+++ lin-vst-server.cpp
@@ -1777,6 +1777,9 @@ void RemoteVSTServer::eventloop()
   x3 = 0;
   y3 = 0;
 #endif
+#ifdef XOFFSET
+  int width2 = 0;
+#endif
 
      if (parent && child) {
      for (int loopidx = 0; (loopidx < 10) && XPending(display); loopidx++) {
@@ -1945,6 +1948,14 @@ void RemoteVSTServer::eventloop()
         XTranslateCoordinates(display, parent, XDefaultRootWindow(display), 0,
                               0, &xmove, &ymove, &ignored);
 
+#ifdef XOFFSET
+        width2 = 0;
+        if(xmove < 0)
+        {
+         width2 = abs(width);
+        }
+#endif	     
+
 #ifdef TRACKTIONWM  
       if(hosttracktion > 0)  
       {   
@@ -1953,9 +1964,14 @@ void RemoteVSTServer::eventloop()
       }
 #endif
 
-           if (hWndvst[pidx] && guiVisible && !exiting) 
-	       MoveWindow(hWndvst[pidx], GetSystemMetrics(SM_XVIRTUALSCREEN) + xmove, GetSystemMetrics(SM_YVIRTUALSCREEN) + ymove, width, height, true);
-
+        if (hWndvst[pidx] && guiVisible && !exiting) 
+	{
+#ifdef XOFFSET	
+        MoveWindow(hWndvst[pidx], GetSystemMetrics(SM_XVIRTUALSCREEN) + xmove, GetSystemMetrics(SM_YVIRTUALSCREEN) + ymove, width + width2, height, true);
+#else	   
+	MoveWindow(hWndvst[pidx], GetSystemMetrics(SM_XVIRTUALSCREEN) + xmove, GetSystemMetrics(SM_YVIRTUALSCREEN) + ymove, width, height, true);
+#endif
+       }
         break;
 
 #ifdef EMBEDDRAG
